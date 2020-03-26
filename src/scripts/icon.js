24f13a0c4ea7c00e6ce6b5dce1e6ee8732307aa7
陳建宇 <chenjianyu@chenjianyude-MacBook-Pro.local>
2020-03-26 22:52:38 +0800

    finish tomato UI version1

--- src/scripts/icon.js
+++ src/scripts/icon.js
@@ -5,14 +5,18 @@ import {
     faPlay,
     faPause,
     faTimes,
-    faBell
+    faBell,
+    faBellSlash,
+    faCheck,
 } from '@fortawesome/free-solid-svg-icons';
 
 const icons = [
     faPlay,
     faPause,
     faTimes,
-    faBell
+    faBell,
+    faBellSlash,
+    faCheck
 ];
 
 function init(){
